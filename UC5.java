5b73d835e8ad78d23cfd6ee052bb85a2edb255e0
bhavya07-alt <bhavya.patel072007@gmail.com>
2026-05-06 08:34:31 +0530

    UC5: added StringBuilder greeting program

---
+++ UC5.java
@@ -0,0 +1,19 @@
+public class UC5 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(name);
+                first = false;
+            }
+            System.out.println("Hello, " + nameBuilder.toString() + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
